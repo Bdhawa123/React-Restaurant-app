fd14f6b8c6bec14d417d2eba7ed7fd7d7fac4712
Binay <binaydhawa@gmail.com>
2019-01-22 12:02:34 +1100

    Functional Changes

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'; 
 import { DISHES } from '../shared/dishes';
 
-
+/*
 class DishdetailComponent extends Component
 {  
     constructor(props )
@@ -65,5 +65,83 @@ class DishdetailComponent extends Component
           );
     }
 }
+*/
+    function RenderDish({dish}){
+        if (dish!=null)
+        {
+            return (
+                <div className = "container">
+                    <div className= "row">
+                    <div className="col-12 col-md-5 m-1 container">
+                        <CardImg width="100%"  src={dish.image} alt ={dish.name}/>
+                        
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </div>
+
+
+                    <div className = "col-12 col-md-5 m-1 container">
+                        <RenderComments Comments ={dish.comments}/>
+                    </div>
+                    </div>
+                </div>
+            );
+        }
+        else
+        {
+            return(
+                <div>
+                </div>
+            );
+        }
+
+    }
+
+    function RenderComments({Comments}){
+
+        if (Comments!=null)
+        {
+            const comm = Comments.map((comment) =>{
+                return(
+                    <div className = "row">
+                        <div className ="col-6">
+                        {comment.comment}
+                        </div>
+                        
+                        <div className = "col-6">
+                        {new Intl.DateTimeFormat('en-AU', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        </div>
+                    </div>
+                
+                );
+            });
+            return(
+                <div>
+                    {comm}
+                </div>
+            );
+        }
+        else
+        {
+            return(
+                <div>
+                    The comment section is empty
+                </div>
+            )
+        }
+
+    }
+    
+    const DishdetailComponent = (props) =>{
+        return (
+            <div>
+                <RenderDish dish={props.dish}/>
+                
+            </div>
+        );
+        
+    }
 
 export default DishdetailComponent;
