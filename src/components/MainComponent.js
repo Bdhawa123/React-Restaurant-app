10afd982854d296799bf2b3534a3a5af5db3e151
Binay <binaydhawa@gmail.com>
2019-02-05 15:59:26 +1100

    Error handling erros present

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
-import { addComment,fetchDishes,fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import {actions } from 'react-redux-form';
  
 import Header from './HeaderComponent';
@@ -23,13 +23,12 @@ const mapStatetoProps = state=> {
     } 
 }
 
-const mapDispatchToProps = (dispatch) =>({
-  addComment:(dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
-  fetchDishes:() => {dispatch(fetchDishes())},
-  resetFeedbackForm:() => { dispatch(actions.reset('feedback'))},
-  fetchComments:() => {dispatch(fetchComments())},
-  fetchPromos:() => {dispatch(fetchPromos())},
-
+const mapDispatchToProps = dispatch => ({
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos())
 });
 
 
@@ -40,7 +39,7 @@ constructor(props){
  
 }
 
-componentDidMount(){
+componentDidMount() {
   this.props.fetchDishes();
   this.props.fetchComments();
   this.props.fetchPromos();
@@ -57,25 +56,26 @@ onDishSelect(dishId){
     const Homepage = () => {
       return(
         <Home 
-              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-              dishesLoading={this.props.dishes.isLoading}
-              dishErrMess={this.props.dishes.errMess}
-              promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-              promoLoading={this.props.promotions.isLoading}
-              promoErrMess={this.props.promotions.errMess}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          />
+        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={this.props.dishes.isLoading}
+        dishErrMess={this.props.dishes.errMess}
+        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+        promoLoading={this.props.promotions.isLoading}
+        promoErrMess={this.props.promotions.errMess}
+        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+    />
       );
     }
 
     const DishWithId = ({match})=> {
       return(
-        <DishdetailComponent dish = {this.props.dishes.dishes.filter((dish)=>dish.id === parseInt(match.params.dishId,10))[0]}
-          isLoading ={this.props.dishes.isLoading}
-          errMess = {this.props.dishes.errMess}
-          commentsErrMsg = {this.props.comments.errMess}
-          comments = {this.props.comments.comments.filter((comment)=> comment.dishId=== parseInt(match.params.dishId,10))}
-          addComment = {this.props.addComment}/>
+        <DishdetailComponent dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        isLoading={this.props.dishes.isLoading}
+        errMess={this.props.dishes.errMess}
+        comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        commentsErrMess={this.props.comments.errMess}
+        addComment={this.props.addComment}
+      />
 
       );
     }
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,4 @@
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
 import {baseUrl } from '../shared/baseUrl';
 
 export const addComment = (dishId,rating, author, comment) =>({
@@ -12,14 +11,30 @@ export const addComment = (dishId,rating, author, comment) =>({
     }
 });
 
-export const fetchDishes = () => (dispatch) =>{
+
+export const fetchDishes = () => (dispatch) => {
+
     dispatch(dishesLoading(true));
 
-    return fetch(baseUrl+'dishes')
-        .then(response=>response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
-
+    return fetch(baseUrl + 'dishes')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
 }
+
 
 export const dishesLoading =() =>({
     type:ActionTypes.DISHES_LOADING
@@ -36,46 +51,71 @@ export const addDishes = (dishes) =>({
     payload:dishes
 });
 
+export const fetchComments = () => (dispatch) => {    
+    return fetch(baseUrl + 'comments')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
+};
 
-export const fetchComments = () => (dispatch) =>{
 
-    return fetch(baseUrl+'comments')
-        .then(response=>response.json())
-        .then(comments => dispatch(addComments(comments)));
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
 
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => {
+    
+    dispatch(promosLoading());
+
+    return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
 }
 
-export const commentsFailed =(errmess) =>({
-    type:ActionTypes.COMMENTS_FAILED,
-    payload:errmess
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
 });
 
-export const addComments = (comments) =>({
-    type:ActionTypes.ADD_COMMENT,
-    payload:comments
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
 });
 
-
-export const fetchPromos = () => (dispatch) =>{
-    dispatch(promosLoading(true));
-
-    return fetch(baseUrl+'promotions')
-        .then(response=>response.json())
-        .then(promos => dispatch(addPromos(promos)));
-
-}
-
-export const promosLoading =() =>({
-    type:ActionTypes.PROMOS_LOADING
-
-});
-
-export const promosFalied =(errmess) =>({
-    type:ActionTypes.PROMOS_FAILED,
-    payload:errmess
-});
-
-export const addPromos = (promos) =>({
-    type:ActionTypes.ADD_PROMOS,
-    payload:promos
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
 });
